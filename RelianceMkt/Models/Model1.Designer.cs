af239e071df6a2aa6a8f4ef2af41fd652df30139
amir12051998 <amir.husain@expnovateur.com>
2026-04-13 14:50:36 +0530

    amir changes

--- RelianceMkt/Models/Model1.Designer.cs
+++ RelianceMkt/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Kamlesh\OfficeProject\Digimyin-main\Digimyin-main (3)\Digimyin-main (3)\Digimyin-main\RelianceMkt\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Amir Husain\source\repos\degymine\RelianceMkt\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
