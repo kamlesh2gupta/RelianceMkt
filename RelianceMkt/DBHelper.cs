dbfbe788637f68fdef5208c0a92600806c839873
amir12051998 <amir.husain@expnovateur.com>
2026-04-14 12:57:26 +0530

    updated by amir

---
+++ RelianceMkt/DBHelper.cs
@@ -0,0 +1,24 @@
+﻿// Helpers/DBHelper.cs
+using System.Configuration;
+
+public static class DBHelper
+{
+    public static string GetConnectionString()
+    {
+        string env = ConfigurationManager.AppSettings["ActiveDB"];
+
+        if (env == "Server")
+        {
+            // Server wali string — same jo server Web.config mein hai
+            return "Data Source=10.126.143.86,1981;Initial Catalog=DIGIMYIN;" +
+                   "User ID=reliance_user;Password=pass@123;" +
+                   "MultipleActiveResultSets=True;Connection Timeout=10000;";
+        }
+        else
+        {
+            // Local wali string
+            return ConfigurationManager
+                .ConnectionStrings["LocalConnection"].ConnectionString;
+        }
+    }
+}
---
+++ RelianceMkt/Models/WhatsAppJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RelianceMkt.Models
+{
+    public class WhatsAppJob
+    {
+        public string Mobile { get; set; }
+        public string Name { get; set; }
+        public string CampaignName { get; set; }
+        public string ImageLink { get; set; }
+        public int RetryCount { get; set; } = 0;
+    }
+}
